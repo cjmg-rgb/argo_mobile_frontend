963329a0f0ddb2501bd746d4e3d3a670628e32eb
sijeyemji <sijeyemji@gmail.com>
2025-03-13 13:07:56 +0800

    Added Dependencies

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const index = () => {
+  return (
+    <View className='flex flex-1 justify-center items-center'>
+      <Text className='text-3xl'>index</Text>
+    </View>
+  )
+}
+
+export default index
